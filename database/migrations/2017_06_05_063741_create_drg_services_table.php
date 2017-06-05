6a6cc1fb49248e99e8ca0bc8c43bbff419193889
PavithraSrinivas <pavithras@kavaii.com>
2017-06-05 12:16:06 +0530

    DRG

--- database/migrations/2017_06_05_063741_create_drg_services_table.php
+++ database/migrations/2017_06_05_063741_create_drg_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePatientsTable extends Migration
+class CreateDrgServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePatientsTable extends Migration
      */
     public function up()
     {
-        Schema::create('patients', function (Blueprint $table) {
+        Schema::create('drg_services', function (Blueprint $table) {
 	$table->increments('id');
     $table->string('title');
 	$table->string('language');
@@ -68,6 +68,6 @@ class CreatePatientsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('patients');
+        Schema::dropIfExists('drg_services');
     }
 }
